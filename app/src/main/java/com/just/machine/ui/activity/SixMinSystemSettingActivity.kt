7e9b383754633904be90dced3a51433bb9de6bcf
robert <luobaixun@163.com>
2024-03-08 16:01:52 +0800

    系统设置更新

--- app/src/main/java/com/just/machine/ui/activity/SixMinSystemSettingActivity.kt
+++ app/src/main/java/com/just/machine/ui/activity/SixMinSystemSettingActivity.kt
@@ -1,5 +1,6 @@
 package com.just.machine.ui.activity
 
+import androidx.core.content.ContextCompat
 import com.common.base.CommonBaseActivity
 import com.just.news.R
 import com.just.news.databinding.ActivitySixminSystemSettingBinding
@@ -7,10 +8,54 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class SixMinSystemSettingActivity : CommonBaseActivity<ActivitySixminSystemSettingBinding>() {
+
+    private var autoCircleCount: Boolean = true
     override fun getViewBinding() = ActivitySixminSystemSettingBinding.inflate(layoutInflater)
 
     override fun initView() {
         initToolbar()
+        initClickListener()
+    }
+
+    private fun initClickListener() {
+        binding.sixminRlAutoCircleCount.setOnClickListener {
+            if (!autoCircleCount) {
+                binding.sixminRlAutoCircleCount.setBackgroundResource(R.drawable.sixmin_circle_select_bg)
+                binding.sixminRlHandleCircleCount.setBackgroundResource(R.drawable.sixmin_circle_unselect_bg)
+                binding.sixminTvAutoCircleCount.setTextColor(
+                    ContextCompat.getColor(
+                        this,
+                        R.color.colorPrimary
+                    )
+                )
+                binding.sixminTvHandleCircleCount.setTextColor(
+                    ContextCompat.getColor(
+                        this,
+                        R.color.text3
+                    )
+                )
+            }
+            autoCircleCount = !autoCircleCount
+        }
+        binding.sixminRlHandleCircleCount.setOnClickListener {
+            if (autoCircleCount) {
+                binding.sixminRlAutoCircleCount.setBackgroundResource(R.drawable.sixmin_circle_unselect_bg)
+                binding.sixminRlHandleCircleCount.setBackgroundResource(R.drawable.sixmin_circle_select_bg)
+                binding.sixminTvAutoCircleCount.setTextColor(
+                    ContextCompat.getColor(
+                        this,
+                        R.color.text3
+                    )
+                )
+                binding.sixminTvHandleCircleCount.setTextColor(
+                    ContextCompat.getColor(
+                        this,
+                        R.color.colorPrimary
+                    )
+                )
+            }
+            autoCircleCount = !autoCircleCount
+        }
     }
 
     private fun initToolbar() {
@@ -20,5 +65,11 @@ class SixMinSystemSettingActivity : CommonBaseActivity<ActivitySixminSystemSetti
 //        binding.toolbar.ivTitleBack.setNoRepeatListener {
 //            finish()
 //        }
+        binding.rbBroadcastGuidanceVoiceYes.isChecked = true
+        binding.rbAutoMearsureBloodYes.isChecked = true
+        binding.rbAutoStartTestYes.isChecked = true
+        binding.rbPositionOfTrolleyMiddle.isChecked = true
+        binding.rbStepsOrBreathSteps.isChecked = true
+        binding.rbShowRestTimeYes.isChecked = true
     }
 }
--- common/src/main/java/com/common/base/CommonBaseActivity.kt
+++ common/src/main/java/com/common/base/CommonBaseActivity.kt
@@ -1,6 +1,11 @@
 package com.common.base
 
 import android.os.Bundle
+import android.os.IBinder
+import android.view.MotionEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.ViewDataBinding
 
@@ -27,5 +32,52 @@ abstract class CommonBaseActivity<VB : ViewDataBinding> : AppCompatActivity() {
     protected val tag: String = CommonBaseActivity::class.java.simpleName
 
     protected abstract fun initView()
+
+    /**
+     * 点击空白区域隐藏键盘.
+     */
+    override fun dispatchTouchEvent(me: MotionEvent): Boolean {
+        if (me.action == MotionEvent.ACTION_DOWN) {  //把操作放在用户点击的时候
+            val v = currentFocus //得到当前页面的焦点,ps:有输入框的页面焦点一般会被输入框占据
+            if (isShouldHideKeyboard(v, me)) { //判断用户点击的是否是输入框以外的区域
+                hideKeyboard(v!!.windowToken) //收起键盘
+            }
+        }
+        return super.dispatchTouchEvent(me)
+    }
+
+    /**
+     * 根据EditText所在坐标和用户点击的坐标相对比，来判断是否隐藏键盘，因为当用户点击EditText时则不能隐藏
+     *
+     * @param v
+     * @param event
+     * @return
+     */
+    private fun isShouldHideKeyboard(v: View?, event: MotionEvent): Boolean {
+        if (v != null && v is EditText) {  //判断得到的焦点控件是否包含EditText
+            val l = intArrayOf(0, 0)
+            v.getLocationInWindow(l)
+            val left = l[0]
+            //得到输入框在屏幕中上下左右的位置
+            val top = l[1]
+            val bottom = top + v.getHeight()
+            val right = left + v.getWidth()
+            return !(event.x > left && event.x < right && event.y > top && event.y < bottom)
+        }
+        // 如果焦点不是EditText则忽略
+        return false
+    }
+
+    /**
+     * 获取InputMethodManager，隐藏软键盘
+     *
+     * @param token
+     */
+    private fun hideKeyboard(token: IBinder?) {
+        if (token != null) {
+            val im = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+            im.hideSoftInputFromWindow(token, InputMethodManager.HIDE_NOT_ALWAYS)
+        }
+    }
 
 }
