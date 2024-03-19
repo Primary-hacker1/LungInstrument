88ee314f1bf6e15a02fbc3968ae7dceecd5a02d6
user <942772119@qq.com>
2024-03-19 15:53:32 +0800

    成分定标ui

---
+++ app/src/main/java/com/just/machine/ui/adapter/calibration/IngredientAdapter.kt
@@ -0,0 +1,41 @@
+package com.just.machine.ui.adapter.calibration
+
+import android.content.Context
+import androidx.core.content.ContextCompat
+import com.common.base.BaseRecyclerViewAdapter
+import com.just.machine.model.calibration.IngredientBean
+import com.just.news.R
+import com.just.news.databinding.ItemIngredientBinding
+
+class IngredientAdapter(val context: Context) :
+    BaseRecyclerViewAdapter<IngredientBean, ItemIngredientBinding>() {
+
+    private var selectedItem = -1
+
+    override fun bindData(item: IngredientBean, position: Int) {
+        binding.bean = item
+
+        if (position == selectedItem) {
+            binding.llItem.setBackgroundColor(ContextCompat.getColor(context, R.color.cf4f5fa))
+        } else {
+            binding.llItem.setBackgroundColor(ContextCompat.getColor(context, R.color.white))
+        }
+    }
+
+    /**
+     * @param position 点击 item 的 position
+     * */
+    fun toggleItemBackground(position: Int) {
+        if (selectedItem == position) {
+            selectedItem = -1
+        } else {
+            selectedItem = position
+        }
+        notifyDataSetChanged()
+    }
+
+
+    override fun getLayoutRes(): Int {
+        return R.layout.item_ingredient
+    }
+}
--- app/src/main/java/com/just/machine/ui/fragment/calibration/IngredientFragment.kt
+++ app/src/main/java/com/just/machine/ui/fragment/calibration/IngredientFragment.kt
@@ -3,9 +3,11 @@ package com.just.machine.ui.fragment.calibration
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.common.base.CommonBaseFragment
+import com.just.machine.model.calibration.IngredientBean
+import com.just.machine.ui.adapter.calibration.IngredientAdapter
 import com.just.machine.ui.viewmodel.MainViewModel
-import com.just.news.databinding.FragmentFlowBinding
 import com.just.news.databinding.FragmentIngredientBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -19,8 +21,44 @@ class IngredientFragment : CommonBaseFragment<FragmentIngredientBinding>() {
 
     private val viewModel by viewModels<MainViewModel>()
 
+    private val ingredientAdapter by lazy {
+        IngredientAdapter(requireContext())
+    }
+
     override fun initView() {
 
+        binding.rvIngredient.layoutManager = LinearLayoutManager(requireContext())
+
+        ingredientAdapter.setItemClickListener { item, position ->
+            ingredientAdapter.toggleItemBackground(position)
+        }
+
+        ingredientAdapter.setItemsBean(
+            mutableListOf
+                (
+                IngredientBean("容积1", "3", "3.003"),
+                IngredientBean("容积1", "3", "3.003"),
+                IngredientBean("容积1", "3", "3.003"),
+            )
+        )
+
+        binding.rvIngredient.adapter = ingredientAdapter
+
+        binding.materialSwitch.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                // SwitchMaterial 打开
+            } else {
+                // SwitchMaterial 关闭
+            }
+        }
+
+        binding.materialSwitch2.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                // SwitchMaterial 打开
+            } else {
+                // SwitchMaterial 关闭
+            }
+        }
     }
 
     override fun initListener() {
