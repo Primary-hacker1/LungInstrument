9717f7f8dcf58c8eea8d8958f7325f3c0b90b729
user <942772119@qq.com>
2024-04-02 16:05:15 +0800

    string

--- app/src/main/java/com/just/machine/ui/adapter/CustomSpinnerAdapter.kt
+++ app/src/main/java/com/just/machine/ui/adapter/CustomSpinnerAdapter.kt
@@ -11,12 +11,15 @@ import com.just.news.R
 class CustomSpinnerAdapter(context: Context, private val dataList: MutableList<Pair<String, String>>? = ArrayList()) :
     ArrayAdapter<Pair<String, String>>(context, R.layout.custom_spinner_item, dataList!!) {
 
-    fun updateSpinnerText(spinnerText: String) {
-        for (i in 0 until dataList!!.size) {
-            dataList[i] = dataList[i].copy(second = spinnerText)
-        }
+    fun updateSpinnerText(dataList: MutableList<Pair<String, String>>) {
+        // 清空适配器原有数据
+        clear()
+        // 将新数据添加到适配器
+        addAll(dataList)
+        // 通知适配器数据已更改
         notifyDataSetChanged()
     }
+
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         return createItemView(position, convertView, parent)
--- app/src/main/java/com/just/machine/ui/fragment/cardiopulmonary/DynamicFragment.kt
+++ app/src/main/java/com/just/machine/ui/fragment/cardiopulmonary/DynamicFragment.kt
@@ -1,16 +1,21 @@
 package com.just.machine.ui.fragment.cardiopulmonary
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
 import com.common.base.CommonBaseFragment
-import com.common.viewmodel.LiveDataEvent
+import com.common.base.onUI
+import com.common.base.setNoRepeatListener
+import com.common.network.LogUtils
 import com.just.machine.ui.adapter.CustomSpinnerAdapter
 import com.just.machine.ui.viewmodel.MainViewModel
 import com.just.machine.util.LiveDataBus
 import com.just.news.databinding.FragmentDynamicBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_calibration.btn_ingredient
 
 
 /**
@@ -44,6 +49,16 @@ class DynamicFragment : CommonBaseFragment<FragmentDynamicBinding>() {
         val adapterTex = CustomSpinnerAdapter(requireContext())
         val adapterFeo2 = CustomSpinnerAdapter(requireContext())
 
+        val adapterTph = CustomSpinnerAdapter(requireContext())
+        val adapterVCO2 = CustomSpinnerAdapter(requireContext())
+        val adapterHrr = CustomSpinnerAdapter(requireContext())
+        val adapterVe = CustomSpinnerAdapter(requireContext())
+        val adapterSpo2 = CustomSpinnerAdapter(requireContext())
+        val adapterFico2 = CustomSpinnerAdapter(requireContext())
+        val adapterBf = CustomSpinnerAdapter(requireContext())
+        val adapterVtex = CustomSpinnerAdapter(requireContext())
+        val adapterFeco2 = CustomSpinnerAdapter(requireContext())
+
         binding.timeSpinner.adapter = adapterTime
         binding.voSpinner.adapter = adapterVo
         binding.hrSpinner.adapter = adapterHr
@@ -54,9 +69,45 @@ class DynamicFragment : CommonBaseFragment<FragmentDynamicBinding>() {
         binding.texSpinner.adapter = adapterTex
         binding.feoSpinner.adapter = adapterFeo2
 
-        LiveDataBus.get().with("msg").observe(this) {//串口消息
-            adapterTime.updateSpinnerText("text")
+        binding.phSpinner.adapter = adapterTph
+        binding.vcoSpinner.adapter = adapterVCO2
+        binding.hrrSpinner.adapter = adapterHrr
+        binding.veSpinner.adapter = adapterVe
+        binding.spoSpinner.adapter = adapterSpo2
+        binding.ficoSpinner.adapter = adapterFico2
+        binding.bfSpinner.adapter = adapterBf
+        binding.vtexSpinner.adapter = adapterVtex
+        binding.fecoSpinner.adapter = adapterFeco2
 
+        LiveDataBus.get().with("msg").observe(this) {//串口消息
+            if (it is ArrayList<*>) {
+                val dataList = it as ArrayList<Pair<String, String>>
+                // 在这里对数据进行解析和处理
+                for (pair in dataList) {
+                    val key = pair.first
+                    val value = pair.second
+                    // 对每个 Pair 中的数据进行相应的操作，例如更新 UI 或执行其他逻辑
+                    LogUtils.d(tag + value)
+                }
+                // 你可以在这里根据需要更新 Adapter 或其他 UI 元素
+                adapterTime.updateSpinnerText(dataList)
+            }
+        }
+
+        var title = 111//模拟串口数据
+        var title1 = 222//模拟串口数据
+
+        binding.llStart.setNoRepeatListener {
+
+            title++
+
+            title1++
+
+            LiveDataBus.get().with("msg").value = mutableListOf(
+                Pair("111", title.toString()),
+                Pair("222", title1.toString()),
+                Pair("333", "item")
+            )
         }
     }
 
